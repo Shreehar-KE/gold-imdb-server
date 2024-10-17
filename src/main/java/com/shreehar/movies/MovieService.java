bcfd0966e7294827c279297762aebea0476650af
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-10-17 12:50:17 +0530

    add CORS for client

--- src/main/java/com/shreehar/movies/MovieService.java
+++ src/main/java/com/shreehar/movies/MovieService.java
@@ -1,10 +1,8 @@
 package com.shreehar.movies;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
